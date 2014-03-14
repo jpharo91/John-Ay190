66a63ea1e8b9d581d74779729bcafaee87042bbb
Cy Ghost <cyghost@gmail.com>
2014-03-14 10:19:01 -0700

    Filled out more helper functions and general tidying up

--- Project/binary.py
+++ Project/binary.py
@@ -4,11 +4,10 @@ import numpy as np
 import matplotlib.pyplot as pl
 import mpl_toolkits.plot3d as mpl3d
 
-# general constants
+# general constants (CGS)
 ggrav = 6.67e-8
 c = 3.0e10
 msun = 1.99e33
-frac = 1.0/6.0
 direcs = [1, 2, 3] # indices corresponding to the three spatial directions
 
 
@@ -25,46 +24,64 @@ def kron_delta(i, j):
     else:
         return 0
 
-def first_time_der(quantity):
+def first_time_der(quantity, it):
     """
-    Function to calculate the first time derivative of `quantity`.
+    Function to calculate the first time derivative of `quantity` at
+    iteration `it`.
     """
+    return (quantity[it] - quantity[it-1]) / dt
 
-
-def second_time_der(quantity):
+def second_time_der(quantity, it):
     """
-    Function to calculate the second time derivative of `quantity`.
+    Function to calculate the second time derivative of `quantity` at
+    iteration `it`.
     """
+    return (quantity[it] - 2*quantity[it-1] + quantity[it-2]) / dt**2
 
-def third_time_der(quantity):
+def third_time_der(quantity, it):
     """
-    Function to calculate the third time derivative of `quantity`.
+    Function to calculate the third time derivative of `quantity` at
+    iteration `it`.
     """
+    return (quantity[it] - 3*quantity[it-1] + 3*quantity[it-2]
+                                              - quantity[it-3]) / dt**3
 
+def eval_e(E, L):
+    return np.sqrt(1. + (2. * E * L**2) / (ggrav * M)**2)
 
-def orbit(E, L, t):
+def eval_p(L):
+    return L**2 / (ggrav * M)
+
+def eval_x(E, L, phi):
     """
     Function to calculate new orbital separation vector.
     """
-    eccentricity = np.sqrt(1.0 + (2.0*E*L*L)/(ggrav*M)**2)
-    p = L*L/(ggrav*M)
-    r = p/(1 - e*np.cos(phi(t))) #Need to define phi function
+    e, p = eval_e(E, L), eval_p(L)
+    r = p / (1 - e * np.cos(phi))
+    return r * np.array([np.cos(phi), np.sin(phi), 0.])
 
-def quadrupole(x):
+
+def eval_quad(x):
     """
-    Function to calculate current reduced quadrupole tensor.
+    Function to calculate new reduced quadrupole tensor.
     """
     I_bar = {1:{}, 2:{}, 3:{}}
     for i in direcs:
         for j in direcs:
-            I_bar[i][j] = mu * (x[i-1] * x[j-1] - kron_delta(i,j) * np.linalg.norm(x) / 3.)
+            I_bar[i][j] = mu * (x[i-1] * x[j-1] - kron_delta(i,j) * np.sum(x**2) / 3.)
     return I_bar
 
-def phi_RHS(r, L):
-    return L / r**2
+def phi_RHS(x, L):
+    return L / np.sum(x**2)
 
-def integrate_RK4(quantities):
+def integrate_RK4():
+    old = new = np.zeros(3)
+    old[0], old[1], old[2] = E, L, phi
 
+    k1 = RHS(x, L)
+
+def RHS(x, L):
+    return np.array([energy_RHS(), momentum_RHS(), phi_RHS(x, L)])
 
 def integrate_phi(phi, r, L):
     k1 = phi_RHS(r, L)
@@ -149,12 +166,14 @@ def wave():
 ####################################
 # main code body
 
-# parameters
+# parameters (CGS)
 npoints = 10000
-t = 0.0
-t_final = 100.0
-m1 = 1.4*msun
-m2 = 1.0*msun
+t = 0.0 # initial time
+t_final = 100.0 # final time
+m1 = 1.4*msun # mass of body 1
+m2 = 1.0*msun # mass of body 2
+init_separation = 1e10 # initial separation
+min_separation = 2e6 # simulation terminates if separation falls beneath this
 
 # set up initial conditions
 M = m1 + m2
@@ -162,33 +181,30 @@ mu = (m1 * m2) / M
 
 times = np.linspace(t, t_final, num=npoints)
 dt = times[1] - times[0]
-x = [1.0, 1.0, 1.0]
-x_mag = np.sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2])
-x_min = 2e6
+x = init_separation * np.array([1., 0., 0.])
 
-# orbital angle
-phi = np.zeros(npoints)
-# total system energy
-energy = np.zeros(npoints)
-# total system angular momentum
-momentum = np.zeros(npoints)
-# system reduced quadrupole tensor (sets up a 3x3 dictionary indexed like quad[i][j])
-quad = {1:{}, 2:{}, 3:{}}
+# array of orbital angle at each time step
+phis = np.zeros(npoints)
+# array total system energy at each time step
+energies = np.zeros(npoints)
+# array of total system angular momentum at each time step
+momenta = np.zeros(npoints)
+# 3x3 tensor of arrays of reduced quadrupole components at each time step
+quadrupoles = {}
 for i in direcs:
+    quadrupoles[i] = {}
     for j in direcs:
-        quad[i][j] = np.zeros(npoints)
-
+        quadrupoles[i][j] = np.zeros(npoints)
 
 ### main loop ###
 for it, t in enumerate(times):
 
-    if x_mag < x_min:
+    if np.sum(x**2) < x_min:
         break
     else:
         energy[it] = integrate_energy()
         momentum[it] = integrate_momentum()
         x = orbit()
-        x_mag = np.sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2])
 
         # Plot stuff
         plt.clf()
