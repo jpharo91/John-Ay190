a0a74f0054482ff8605893c0e0129ec795605b44
Cy Ghost <cyghost@gmail.com>
2014-03-14 11:47:33 -0700

    Totally revamped the code based on an email from Roland. Now we evolve the major axis a and the eccentricity e, not the energy and the angular momentum.

--- Project/binary.py
+++ Project/binary.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python
 
+"""
+John Pharo and Cutter Coryell
+Caltech Ay 190 Winter 2014 with Christian Ott
+Final Project
+
+Simulates the orbit of a binary system as it decays due to gravitational wave
+emission. 
+"""
+
 import numpy as np
 import matplotlib.pyplot as pl
 import mpl_toolkits.plot3d as mpl3d
+
+####################################
+# Constants and Parameters
 
 # general constants (CGS)
 ggrav = 6.67e-8
 c = 3.0e10
 msun = 1.99e33
-direcs = [1, 2, 3] # indices corresponding to the three spatial directions
+direcs = [0, 1, 2] # indices corresponding to the three spatial directions
+
+# simulation parameters
+npoints = 10000
+integrate = integrate_RK4
+
+# system parameters (CGS)
+t = 0.0 # initial time
+t_final = 100.0 # final time
+init_major_axis = 1e10
+init_eccentricity = 0.
+m1 = 1.4*msun # mass of body 1
+m2 = 1.0*msun # mass of body 2
+min_separation = 2e6 # simulation terminates if separation falls beneath this
 
 
 ####################################
-# helper functions
+# Helper Functions
 
 def kron_delta(i, j):
     """
@@ -27,135 +52,90 @@ def kron_delta(i, j):
 def first_time_der(quantity, it):
     """
     Function to calculate the first time derivative of `quantity` at
-    iteration `it`.
+    iteration `it`. If not enough data to compute, returns 0.
     """
-    return (quantity[it] - quantity[it-1]) / dt
+    try:
+        return (quantity[it] - quantity[it-1]) / dt
+    except IndexError:
+        return 0
 
 def second_time_der(quantity, it):
     """
     Function to calculate the second time derivative of `quantity` at
-    iteration `it`.
+    iteration `it`.  If not enough data to compute, returns 0.
     """
-    return (quantity[it] - 2*quantity[it-1] + quantity[it-2]) / dt**2
+    try:
+        return (quantity[it] - 2*quantity[it-1] + quantity[it-2]) / dt**2
+    except IndexError:
+        return 0
 
 def third_time_der(quantity, it):
     """
     Function to calculate the third time derivative of `quantity` at
-    iteration `it`.
+    iteration `it`.  If not enough data to compute, returns 0.
     """
-    return (quantity[it] - 3*quantity[it-1] + 3*quantity[it-2]
-                                              - quantity[it-3]) / dt**3
+    try:
+        return (quantity[it] - 3*quantity[it-1] + 3*quantity[it-2]
+                                                  - quantity[it-3]) / dt**3
+    except IndexError:
+        return 0
 
-def eval_e(E, L):
-    return np.sqrt(1. + (2. * E * L**2) / (ggrav * M)**2)
-
-def eval_p(L):
-    return L**2 / (ggrav * M)
-
-def eval_x(E, L, phi):
+def eval_x(phi, a, e):
     """
     Function to calculate new orbital separation vector.
     """
-    e, p = eval_e(E, L), eval_p(L)
-    r = p / (1 - e * np.cos(phi))
-    return r * np.array([np.cos(phi), np.sin(phi), 0.])
-
+    return eval_r(phi, a, e) * np.array([np.cos(phi), np.sin(phi), 0.])
 
 def eval_quad(x):
     """
     Function to calculate new reduced quadrupole tensor.
     """
-    I_bar = {1:{}, 2:{}, 3:{}}
+    I_bar = np.zeros((3, 3))
+    xnormsq = np.sum(x**2)
     for i in direcs:
         for j in direcs:
-            I_bar[i][j] = mu * (x[i-1] * x[j-1] - kron_delta(i,j) * np.sum(x**2) / 3.)
+            I_bar[i,j] = mu * (x[i] * x[j] - kron_delta(i,j) * xnormsq / 3.)
     return I_bar
 
-def phi_RHS(x, L):
-    return L / np.sum(x**2)
 
-def integrate_RK4():
-    old = new = np.zeros(3)
-    old[0], old[1], old[2] = E, L, phi
-
-    k1 = RHS(x, L)
-
-def RHS(x, L):
-    return np.array([energy_RHS(), momentum_RHS(), phi_RHS(x, L)])
-
-def integrate_phi(phi, r, L):
-    k1 = phi_RHS(r, L)
-    k2 = phi_RHS(tensor + k1/2.0) #Not sure if t is actually necessary
-    k3 = energyRHS(t + dt/2.0, tensor + k2/2.0) #Maybe need function to give tensor
-    k4 = energyRHS(t + dt, tensor + k3)         # as a function of time
-
-
-def energy_RHS(tensor):
+def integrate_RK4(phi, a, e):
     """
-    Function to calculate the righthand-side of the energy evolution equation.
+    Integrates the orbital angle phi, the major axis a, and the eccentricity e
+    with the 4th-order Runge-Kutta method.
     """
-    temp = third_time_der(tensor)
-    coeff = ggrav / (5. * c**5)
-    return coeff * temp * temp
+    old = np.array([phi, a, e])
+    k1 = RHS(old)
+    k2 = RHS(old + 0.5 * k1)
+    k3 = RHS(old + 0.5 * k2)
+    k4 = RHS(old + k3)
+    new = old + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6.
+    return (new[0], new[1], new[2])
 
-def perm_parity(lst):
+def RHS(quants):
     """
-    Given a permutation of the digits 0..N in order as a list, 
-    returns its parity (or sign): +1 for even parity; -1 for odd.
+    Provides a length-3 array of the righthand-sides (time-derivatives)
+    of phi, a, and e for used in integration schemes. `quants` is a length-3
+    sequence with first element phi, second element a, and third element e.
     """
-    parity = 1
-    for i in range(0,len(lst)-1):
-        if lst[i] != i:
-            parity *= -1
-            mn = min(range(i,len(lst)), key=lst.__getitem__)
-            lst[i],lst[mn] = lst[mn],lst[i]
-    return parity
+    phi, a, e = quants[0], quants[1], quants[2]
+    r = eval_r(phi, a, e)
+    return np.array([phi_RHS(r, a, e), a_RHS(a, e), e_RHS(a, e)])
 
-def lc(i, j, k):
-    """
-    Function to calculate the Levi-Civita tensor \( \epsilon_{i,j,k} \).
-    """
-    if i == j or i == k or j == k:
-        return 0
-    else:
-        return perm_parity([i,j,k])
+def eval_r(phi, a, e):
+    return a * (1 - e**2) / (1 + e * np.cos(phi))
 
-def momentum_RHS(tensor, axis):
-    """
-    Function to calculate the righthand-side of the angular momentum evolution equation.
-    """
-    coeff = (2. * ggrav) / (5. * c**5)
-    direcs = [1, 2, 3] # three spatial directions
-    total = 0.
-    for j in direcs:
-        for k in direcs:
-            for m in direcs:
-                epsilon = lc(axis, j, k)
-                total += (coeff * epsilon * second_time_der(tensor[j][m])
-                           * third_time_der(tensor[k][m]))
-    return coeff * total
+def phi_RHS(r, a, e):
+    return np.sqrt(ggrav * M * a * (1 - e**2)) / r**2
 
-def integrate_energy(tensor, t, E):
-    """
-    Function to calculate the righthand-side of the energy evolution equation
-    """
-    k1 = energyRHS(quadrupole(t))
-    k2 = energyRHS(tensor + k1/2.0) #Not sure if t is actually necessary
-    k3 = energyRHS(t + dt/2.0, tensor + k2/2.0) #Maybe need function to give tensor
-    k4 = energyRHS(t + dt, tensor + k3)         # as a function of time
+def a_RHS(a, e):
+    return ((-64./5.) * (ggrav**3 * m1 * m2 * M)
+            * (1 + (73./24.) * e**2 + (37./96.) * e**4) 
+            / (c**5 * a**3 * (1 - e**2)**3.5))
 
-    return E + dt * (k1 + 2.*k2 + 2.*k3 + k4) / 6.
+def e_RHS(a, e):
+    return (-304./15.) * e * (ggrav**3 * m1 * m2 * M)
+            * (1 + (121./304.) * e**2) / (c**5 * a**4 * (1 - e**2)**2.5)
 
-def integrate_momentum():
-    """
-    Function to calculate the righthand-side of the angular momentum evolution equation
-    """
-    k1 = momentum_RHS()
-    k2 = momentum_RHS()
-    k3 = momentum_RHS()
-    k4 = momentum_RHS()
-
-    return L + dt * (k1 + 2.*k2 + 2.*k3 + k4) / 6.
 
 def wave():
     """
@@ -166,54 +146,62 @@ def wave():
 ####################################
 # main code body
 
-# parameters (CGS)
-npoints = 10000
-t = 0.0 # initial time
-t_final = 100.0 # final time
-m1 = 1.4*msun # mass of body 1
-m2 = 1.0*msun # mass of body 2
-init_separation = 1e10 # initial separation
-min_separation = 2e6 # simulation terminates if separation falls beneath this
 
 # set up initial conditions
+
 M = m1 + m2
 mu = (m1 * m2) / M
 
 times = np.linspace(t, t_final, num=npoints)
 dt = times[1] - times[0]
-x = init_separation * np.array([1., 0., 0.])
 
 # array of orbital angle at each time step
 phis = np.zeros(npoints)
-# array total system energy at each time step
-energies = np.zeros(npoints)
-# array of total system angular momentum at each time step
-momenta = np.zeros(npoints)
-# 3x3 tensor of arrays of reduced quadrupole components at each time step
-quadrupoles = {}
-for i in direcs:
-    quadrupoles[i] = {}
-    for j in direcs:
-        quadrupoles[i][j] = np.zeros(npoints)
+
+# array of major axis at each time step
+axs = np.zeros(npoints)
+axs[0] = init_major_axis
+
+# array of eccentricity at each time step
+eccs = np.zeros(npoints)
+eccs[0] = init_eccentricity
+
+# array of separation vector at each time step
+xs = np.zeros((3, npoints))
+xs[:,0] = eval_x(phis[0], axs[0], eccs[0])
+
+# array of reduced quadrupole tensor at each time step
+quads = np.zeros((3, 3, npoints))
+
+# array of plus-polarized gravitational wave strain at each time step
+hpluses = np.zeros(npoints)
+
+# array of cross-polarized gravitational wave strain at each time step
+hcrosses = np.zeros(npoints)
+
 
 ### main loop ###
-for it, t in enumerate(times):
+for it, t in enumerate(times[:,-1]):
 
-    if np.sum(x**2) < x_min:
+    # Update quantities
+    phis[it+1], axs[it+1], eccs[it+1] = integrate(phis[it], axs[it], eccs[it])
+    xs[:,it+1] = eval_x(phis[it+1], axs[it+1], eccs[it+1])
+    quads[:,:,it+1] = eval_quad(xs[it+1])
+    r = np.linalg.norm(xs[:,it+1])
+    hpluses[it+1] = second_time_der(quads[0,0,:] - quads[1,1,:], it+1) / r
+    hcrosses[it+1] = 2 * second_time_der(quads[0,1,:]) / r
+
+    # Plot stuff
+    plt.clf()
+    fig = plt.gcf()
+    ax = mpl3d.Axes3D(fig)
+    ax.scatter(x[0], x[1], x[2]) #Proably need to make x an np array
+    ax.set_xlim((-rmax,rmax)) #Need to define rmax, probably 1.1 times initial x, or something
+    ax.set_ylim((-rmax,rmax))
+    ax.set_zlim((-rmax,rmax))
+    pl.draw()
+
+    if eval_r(phi, a, e) < x_min:
         break
-    else:
-        energy[it] = integrate_energy()
-        momentum[it] = integrate_momentum()
-        x = orbit()
-
-        # Plot stuff
-        plt.clf()
-        fig = plt.gcf()
-        ax = mpl3d.Axes3D(fig)
-        ax.scatter(x[0], x[1], x[2]) #Proably need to make x an np array
-        ax.set_xlim((-rmax,rmax)) #Need to define rmax, probably 1.1 times initial x, or something
-        ax.set_ylim((-rmax,rmax))
-        ax.set_zlim((-rmax,rmax))
-        pl.draw()
 
 pl.show()
